c7ccfee6cc1dbdb127bd79bf35a587916ebe780a
unknown <st4603@utr.edu.mx>
2026-06-22 09:15:25 -0600

    Base configuration

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("API Pasarela de Comercio Segura");
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5100;
+
+app.listen(PORT, () => {
+    console.log("Hello World");
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
