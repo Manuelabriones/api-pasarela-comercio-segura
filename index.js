0e545556125a29bf0898658b44ecdfac2906e4a7
unknown <st4603@utr.edu.mx>
2026-06-30 16:23:24 -0600

    Agrega conexión a MongoDB Atlas

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 
 const express = require("express");
+const helmet = require("helmet");
+const connectDB = require("./src/config/database");
 
 const app = express();
 
 const PORT = process.env.PORT || 5100;
+
+// Conectar a MongoDB
+connectDB();
+
+// Middlewares
+app.use(helmet());
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log("Hello World");
